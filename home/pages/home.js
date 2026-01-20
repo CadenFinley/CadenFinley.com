d333fa8064cdb022a68c8a1ae2c690a0a006da36
CadenFinley <caden.finley0789@gmail.com>
2026-01-19 19:20:39 -0600

    Update home.js

--- home/pages/home.js
+++ home/pages/home.js
@@ -1,16 +1,15 @@
 import {educationSection} from '../components/education.js';
-// import { skillsSection } from "../components/skills.js";
 import {experienceSection} from '../components/experience.js';
 import {footerSection} from '../components/footer.js';
 import {heroSection} from '../components/hero.js';
 import {navSection} from '../components/nav.js';
 import {projectsSection} from '../components/projects.js';
+import {skillsSection} from '../components/skills.js';
 import {tag} from '../scripts/grecha.js';
 
 export function homePage() {
-  return tag('div', navSection(), heroSection(),
-             // skillsSection(),
-             projectsSection(), experienceSection(), educationSection(),
+  return tag('div', navSection(), heroSection(), projectsSection(),
+             experienceSection(), educationSection(), skillsSection(),
              footerSection())
       .att$('class', 'container');
 }
